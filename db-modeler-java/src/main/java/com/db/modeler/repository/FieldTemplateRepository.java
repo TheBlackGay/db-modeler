9bfd7a4827324377360f1bd19715362283ed0c48
钟佳峰 <zhongjiafeng@bwton.com>
2024-12-10 19:19:05 +0800

    feat: 添加JPA和数据库相关配置

--- db-modeler-java/src/main/java/com/db/modeler/repository/FieldTemplateRepository.java
+++ db-modeler-java/src/main/java/com/db/modeler/repository/FieldTemplateRepository.java
@@ -1,1 +1,13 @@
- 
+package com.db.modeler.repository;
+
+import com.db.modeler.model.FieldTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FieldTemplateRepository extends JpaRepository<FieldTemplate, String> {
+    List<FieldTemplate> findByCategory(FieldTemplate.Category category);
+    List<FieldTemplate> findByTagsContaining(String tag);
+} 
